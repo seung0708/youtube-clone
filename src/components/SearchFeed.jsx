78a391d2823b31a3fde0bbdd9798c254a3f45923
Seung Hyun Kim <seung.kim0708@gmail.com>
2023-01-06 21:47:58 -0800

    finished youtube clone

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom';
+import {Box, Typography} from '@mui/material';
+import {Videos} from './';
+
+
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const SearchFeed = () => {
+
+  const [videos, setVideos] = useState([]);
+  const {searchTerm} = useParams();
+  // By using useEffect, we want to call fetchFromAPI as soon as the feed loads to immediately fetch the data
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then(data => setVideos(data.items))
+  }, [searchTerm])
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+    <Typography variant='h4' fontWeight='bold' mb={2} sx={{color: 'white'}}>
+      Search Results For: <span style={{color: '#F31503'}}>{searchTerm}</span> videos
+    </Typography>
+
+    <Videos videos={videos} />
+  </Box>
   )
 }
 
